33981c96adab460922185b9706011c5ac6f1ab6d
Eugene Syrotiuk <yevhen.syrotiuk@accenture.com>
2023-01-11 23:44:18 +0100

    Add room dimentions labels rendering

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -1,7 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
+using TMPro;
 
-public class Line : MonoBehaviour {
+public class Line : MonoBehaviour
+{
 
-    public GameObject startNode, endNode;
+    public GameObject startNode, endNode, sizeLabel;
+    public string name;
+
+    public void RenderLineSizeLabel(Vector3 startPoint, Vector3 endPoint, Transform parent)
+    {
+        TextMeshPro textMesh;
+        
+        if (sizeLabel == null)
+        {
+            sizeLabel = new GameObject(name + " Label");
+            sizeLabel.transform.parent = parent;
+            textMesh = sizeLabel.AddComponent<TextMeshPro>();
+        }
+        else
+        {
+            textMesh = sizeLabel.GetComponent<TextMeshPro>();
+        }
+
+        if (startPoint == null)
+        {
+            startPoint = startNode.transform.position;
+        }
+
+        textMesh.transform.position = Vector3.Lerp(startPoint, endPoint, 0.5f);
+        textMesh.color = Color.black;
+        textMesh.alignment = TextAlignmentOptions.Center;
+        textMesh.fontSize = 12;
+
+        textMesh.text = (Vector3.Distance(startPoint, endPoint) / 5).ToString("0.00") + "m";
+    }
+
+    private void OnDestroy()
+    {
+        UnityEngine.Object.Destroy(sizeLabel);
+    }
 }
